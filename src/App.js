092176cbc630b14a612e5df18aff066c4fb85e27
FikretDuzceker <fikretduzceker5@gmail.com>
2022-11-28 14:00:06 +0300

    Amount eklendi

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       price: "399.99",
       image:
         "https://dl.airtable.com/.attachments/64b266ad865098befbda3c3577a773c9/24497852/yedjpkwxljtb75t3tezl.png",
-      amount: 1,
+      amount: 0,
       info: "sds",
     },
     {
@@ -20,7 +20,7 @@ function App() {
       price: "499.99",
       image:
         "https://dl.airtable.com/.attachments/91c88ae8c1580e2b762ecb3f73ed1eed/a633139a/phone-1_gvesln.png",
-      amount: 1,
+      amount: 0,
       info: "sds",
     },
     {
@@ -29,7 +29,7 @@ function App() {
       price: "699.99",
       image:
         "https://dl.airtable.com/.attachments/bae9208dc34f35128749ecda5b999e84/337c285d/phone-3_h2s6fo.png",
-      amount: 1,
+      amount: 0,
       info: "sds",
     },
     {
@@ -38,14 +38,14 @@ function App() {
       price: "599.99 ",
       image:
         "https://dl.airtable.com/.attachments/91ee456448cef47deec553a2ea3fa8ad/b08bec68/phone-2_ohtt5s.png",
-      amount: 1,
+      amount: 0,
       info: "sds",
     },
   ];
   const [products, setProducts] = useState(initialState);
   const [basket, setBasket] = useState([]);
   const [tutar, setTutar] = useState(0);
-
+  const array = [...basket];
   return (
     <div className="App">
       <h1>React Sepet Uygulaması</h1>
@@ -63,7 +63,6 @@ function App() {
                 <FaPlusSquare
                   style={{ fontSize: "20px" }}
                   onClick={() => {
-                    const array = [...basket];
                     if (
                       array.findIndex((ind) => {
                         return product.id === ind.id;
@@ -77,13 +76,14 @@ function App() {
                         if (item.id === product.id) {
                           return (product.amount += 1);
                         }
-                        setTutar(product.price * product.amount);
+                        setTutar(product.price * (product.amount + 1));
                         setBasket(array);
                       });
                     }
                     console.log(basket);
                   }}
                 />
+                {product.amount}
                 <FaMinusSquare style={{ fontSize: "20px" }} />
               </div>
             </div>
@@ -99,11 +99,11 @@ function App() {
                 {product.title +
                   "  " +
                   "----> " +
-                  product.amount +
+                  (product.amount + 1) +
                   "  " +
                   "Adet:" +
                   "Toplam Tutar" +
-                  (product.amount * product.price).toFixed(2)}
+                  ((product.amount + 1) * product.price).toFixed(2)}
               </li>
             );
           })}
