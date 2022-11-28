e5d420e2658d7d4222cacb875a06c9efa0695a40
FikretDuzceker <fikretduzceker5@gmail.com>
2022-11-28 14:00:40 +0300

    button eklendi

--- src/components/cardInfo/CardInfo.jsx
+++ src/components/cardInfo/CardInfo.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./cardInfo.css";
 function CardInfo({ price }) {
-  return <div className="card-info">{price}</div>;
+  return <div className="card-info">${price} </div>;
 }
 
 export default CardInfo;
